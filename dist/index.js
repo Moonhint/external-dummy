2af3065686d64666cf4d6efebb8f23081d9cbf4e
Antoni <antoni@bukalapak.com>
2021-03-25 17:54:53 +0700

    add widget script

---
+++ dist/index.js
@@ -0,0 +1,55 @@
+const createTag = ({ tagName, style = {}, attributes = {} }) => {
+  const domElm = document.createElement(tagName);
+  Object.assign(domElm.style, style);
+  Object.keys(attributes).forEach((attributeKey) => {
+      domElm.setAttribute(attributeKey, attributes[attributeKey]);
+  });
+  return domElm;
+};
+
+function myFunction(){
+  const dynamicBannersElm = document.getElementById(
+      'bl-modal'
+  );
+
+  // const iframeSrc = https://m.bukalapak.com/kumpulink/u/storepick;
+  
+  const iframeSrc = 'https://review.bukalapak.com/widgets/dynamic_banners_v2?product-category-id=8&keywords=iphone&dimension=760x365&sort=bestratingratio&rating=3&utm=source%3Dkly%2Cmedium%3Dreferral-affiliate&theme=&compact-product=false&show-banner=true&logo-style=dark&background-color=%23ffffff';
+  
+  const iframeContainer = createTag({
+  tagName: 'div',
+  style: {
+      position: 'relative',
+      overflow: 'hidden',
+      width: '100%',
+      height: '610px',
+  }
+  });
+  const iframe = createTag({
+  tagName: 'iframe',
+  style: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      width: '100%',
+      height: '100%',
+      border: 0,
+      overflow: 'hidden'
+  },
+  attributes: {
+      scrolling: 'no',
+      loading: 'lazy'
+  }
+  });
+
+  if (!navigator.userAgent.includes('MSIE')) {
+  iframe.src = iframeSrc;
+  } else {
+  iframe.location = iframeSrc;
+  }
+
+  iframeContainer.appendChild(iframe);
+  dynamicBannersElm.appendChild(iframeContainer);
+}
